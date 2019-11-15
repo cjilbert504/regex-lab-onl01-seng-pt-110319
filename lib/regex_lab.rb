3ec3e4d0e235781530d592251995802ad645e42f
Collin Jilbert <cjilbert504@gmail.com>
2019-11-15 18:36:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\b[aeiouAEIOU]/)? true : false
+  word.map do |wrd|
+  wrd.scan(/\b[aeiouAEIOU]/)? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
